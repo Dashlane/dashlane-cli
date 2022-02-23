1de7280596d1fab33dd85cd5731f7ffc3905510a
Paul-Louis Hery <paul-louis.hery@dashlane.com>
2022-02-23 17:29:47 +0100

    update db path

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 #!/usr/bin/env node
 import sqlite3 from 'sqlite3';
 import { promisify } from 'util';
+import * as fs from 'fs';
 import { registerDevice } from './middleware/registerDevice.js';
 import { sync } from './middleware/sync.js';
 import { getPassword } from './middleware/get.js';
 import { prepareDB } from './middleware/prepareDB.js';
-import path from 'path';
 
 const command = process.argv[2];
 const commandsParameters = process.argv.slice(3);
 
-const login = 'apps@pixelswap.fr';
+const login = 'paullouis.hery@gmail.com';
+
+// The most appropriate folder to store the user's data, by OS
+const USER_DATA_PATH = process.env.APPDATA || (process.platform === 'darwin' ?
+        process.env.HOME + '/Library/Application Support' :
+        process.env.HOME + '/.local/share'
+);
+const DB_PATH = USER_DATA_PATH + '/dashlane-cli';
 
 const run = async () => {
-    // create the db file if it doesn't exist
-    const DB_PATH = path.resolve(new URL('', import.meta.url).pathname, '../../db/database.db');
+    // create the data folder if it doesn't exist
+    if (!fs.existsSync(DB_PATH)) {
+        fs.mkdirSync(DB_PATH, { recursive: true });
+    }
+
     const db = await promisify<sqlite3.Database>(cb => {
         const db: sqlite3.Database =
-            new sqlite3.Database(DB_PATH, (err) => cb(err, db));
+            new sqlite3.Database(DB_PATH + '/userdata.db', (err) => cb(err, db));
     })();
     console.log('Connected to database.');
 
