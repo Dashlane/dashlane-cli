7a520d925819115bc6dc6f5fe6f983126b2fea82
Paul-Louis Hery <paul-louis.hery@dashlane.com>
2022-04-12 16:45:45 +0200

    feat(totp): log the totp when it's stored with the password

--- src/types.ts
+++ src/types.ts
@@ -62,8 +62,35 @@ export interface AuthentifiantTransactionContent {
         KWAuthentifiant: {
             KWDataItem: {
                 key: string;
-                $t: string
+                $t?: string;
             }[]
         }
     };
+}
+
+export interface VaultCredential {
+    title: string;
+    email?: string;
+    login?: string;
+    password: string;
+    url: string;
+    secondaryLogin?: string;
+    category?: string;
+    note?: string;
+    lastBackupTime: string; // timestamp
+    autoProtected: 'true' | 'false';
+    autoLogin: 'true' | 'false';
+    subdomainOnly: 'true' | 'false';
+    useFixedUrl: 'true' | 'false';
+    otpSecret?: string;
+    appMetaData?: string; // info about linked mobile applications
+    status: 'ACCOUNT_NOT_VERIFIED' | 'ACCOUNT_VERIFIED' | 'ACCOUNT_INVALID';
+    numberUse: string; // number
+    lastUse: string; // timestamp
+    strength: string; // number between 0 and 100
+    modificationDatetime: string; // timestamp
+    checked: 'true' | 'false';
+    id: string;
+    anonId: string;
+    localeFormat: string; // either UNIVERSAL or a country code
 }
