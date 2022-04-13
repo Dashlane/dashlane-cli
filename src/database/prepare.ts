57e45c69d22a21464bd4a0b4b31cf7485e6076cb
Jérôme Boillot <jerome.boillot@dashlane.com>
2022-04-13 18:10:12 +0200

    Run the prepared statements while creating database

--- src/database/prepare.ts
+++ src/database/prepare.ts
@@ -14,12 +14,12 @@ export const prepareDB = (params: PrepareDB): DeviceKeysWithLogin | null => {
             type VARCHAR(255) NOT NULL,
             action VARCHAT(255) NOT NULL,
             content BLOB
-        );`);
+        );`).run();
     db.prepare(`CREATE TABLE IF NOT EXISTS device (
             login VARCHAR(255) PRIMARY KEY,
             accessKey VARCHAR(255) NOT NULL,
             secretKey VARCHAR(255) NOT NULL
-        );`);
+        );`).run();
 
     return db.prepare('SELECT * FROM device LIMIT 1').get() as (DeviceKeysWithLogin | null);
 };
