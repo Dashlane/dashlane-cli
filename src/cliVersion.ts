91bf8f8e84421eccd3f79101fba02745f638d61f
Corentin Mors <corentin@dashlane.com>
2022-07-15 11:58:16 +0200

    CLI version might be invalid (#32)

--- src/cliVersion.ts
+++ src/cliVersion.ts
@@ -7,10 +7,13 @@ export const cliVersionToString = (version: CliVersion): string => {
     return `${version.major}.${version.minor}.${version.patch}`;
 };
 
-export const stringToCliVersion = (version: string): CliVersion => {
+export const stringToCliVersion = (version: string): CliVersion | Error => {
+    if (!version) {
+        return new Error(`No version provided`);
+    }
     const versionSegments = version.split('.', 3).map((segment) => parseInt(segment, 10));
     if (versionSegments.length !== 3 || versionSegments.findIndex(isNaN) >= 0) {
-        throw new Error(`This version string is invalid: ${version}`);
+        return new Error(`This version string is invalid: ${version}`);
     }
     return { major: versionSegments[0], minor: versionSegments[1], patch: versionSegments[2] };
 };
