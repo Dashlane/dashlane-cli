cf54864a47a099f6a664179f58b850a66d06d30c
Paul Tournemaine <paultournemaine@gmail.com>
2023-03-28 10:46:00 +0200

    add get audit log command (#92)
    Add the audit log command with limit and page params
    
    Co-authored-by: Paul Tournemaine <paul.tournemaine@dashlane.com>

---
+++ src/endpoints/getAuditLogs.ts
@@ -0,0 +1,30 @@
+import { requestUserApi } from '../requestApi';
+import { Secrets } from '../types';
+
+interface GetTeamMembersParams {
+    secrets: Secrets;
+    page: number;
+    limit: number;
+}
+
+export const getAuditLogs = (params: GetTeamMembersParams) => {
+    return requestUserApi<GetAuditLogsOutput[]>({
+        path: 'teams/GetAuditLogs',
+        login: params.secrets.login,
+        deviceKeys: {
+            accessKey: params.secrets.accessKey,
+            secretKey: params.secrets.secretKey,
+        },
+        payload: {
+            pageNumber: params.page,
+            pageCount: params.limit,
+        },
+    });
+};
+
+export interface GetAuditLogsOutput {
+    /**
+     * Array of audit logs
+     */
+    auditLogs: string[];
+}
