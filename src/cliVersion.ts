e463b51b0f2ba2492b710a77caba91624330ec1b
Corentin Mors <corentin@dashlane.com>
2023-07-31 18:29:05 +0200

    Bump version to 1.9.0

--- src/cliVersion.ts
+++ src/cliVersion.ts
@@ -1,6 +1,6 @@
 import { CliVersion } from './types';
 
-export const CLI_VERSION: CliVersion = { major: 1, minor: 8, patch: 0 };
+export const CLI_VERSION: CliVersion = { major: 1, minor: 9, patch: 0 };
 export const breakingChangesVersions: CliVersion[] = [];
 
 export const cliVersionToString = (version: CliVersion): string => {
