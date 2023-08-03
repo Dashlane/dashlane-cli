52c4ab62b22ce1ba6f8c8d529cf60b961e1556e1
Corentin Mors <corentin@dashlane.com>
2023-08-03 11:06:01 +0200

    Export team commands as CSV
    Fix #144

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,6 @@
+import { Parser } from '@json2csv/plainjs';
+import { flatten } from '@json2csv/transforms';
+
 export const notEmpty = <TValue>(value: TValue | null | undefined): value is TValue => {
     return value !== null && value !== undefined;
 };
@@ -22,4 +25,16 @@ export const removeUnderscoresAndCapitalize = (string: string): string => {
 /** Unix timestamp to human readable string */
 export const unixTimestampToHumanReadable = (timestamp: number | null): string => {
     return timestamp ? new Date(timestamp * 1000).toLocaleString() : 'N/A';
+};
+
+export const jsonToCsv = (json: Record<string, any>): string => {
+    const parser = new Parser();
+    const csv = parser.parse(json);
+    return csv;
+};
+
+export const flattenJsonArrayOfObject = (json: Record<string, any>[]): Record<string, any> => {
+    const flattenTransform = flatten();
+    const flattenedJson = json.map((entry) => flattenTransform(entry));
+    return flattenedJson;
 };
