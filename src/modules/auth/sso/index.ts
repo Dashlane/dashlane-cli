345dc6eedbdd917e6802d7322283fac3dd56510a
Corentin Mors <corentin@dashlane.com>
2023-09-05 11:39:08 +0200

    Update to node18 and yarn3.6

--- src/modules/auth/sso/index.ts
+++ src/modules/auth/sso/index.ts
@@ -1,4 +1,4 @@
-import { chromium } from 'playwright';
+import { chromium } from 'playwright-core';
 import { DASHLANE_APP_REGEX, extractSsoInfoFromUrl } from './utils';
 import { performSSOVerification } from '../../../endpoints/performSSOVerification';
 
