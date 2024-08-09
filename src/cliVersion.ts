6bf6b2170ec9ab5ae560a2ddda8cbba05a71c815
Corentin Mors <corentin@dashlane.com>
2024-08-09 14:51:30 +0200

    Bump version to v6.2432.0

--- src/cliVersion.ts
+++ src/cliVersion.ts
@@ -1,6 +1,6 @@
 import { CliVersion } from './types.js';
 
-export const CLI_VERSION: CliVersion = { major: 6, minor: 2424, patch: 2 };
+export const CLI_VERSION: CliVersion = { major: 6, minor: 2432, patch: 0 };
 export const breakingChangesVersions: CliVersion[] = [];
 
 export const cliVersionToString = (version: CliVersion): string => {
