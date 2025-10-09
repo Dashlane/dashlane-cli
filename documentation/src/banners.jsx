2b0cd8dfced8c22fa08911c4dbbcf7e673b85337
arenault-dashlane <antoine.renault@dashlane.com>
2025-10-09 11:36:21 +0200

    doc(enrolledDevice): update documentation for replacing teamDevice by enrolledDevice authentication
    - Update banners
    - Update env variables
    - Update flows

--- documentation/src/banners.jsx
+++ documentation/src/banners.jsx
@@ -1,14 +1,20 @@
 import { Callout } from 'nextra/components';
 import Link from 'next/link';
 
-export const TeamCredentialsBanner = () => {
+export const EnrolledDeviceCredentialsBanner = () => {
     return (
-        <Callout emoji="💡">This command needs team credentials to be used. See <Link href='/business' style={{ textDecoration: 'underline' }}>team credentials generation</Link>.</Callout>
+        <Callout emoji="💡">This command needs CLI keys to be used. See <Link href='/business' style={{ textDecoration: 'underline' }}>CLI keys generation</Link>.</Callout>
     );
 };
 
 export const AdminBanner = () => {
     return (
         <Callout emoji="🔒">This command is only available to team administrators.</Callout>
+    );
+};
+
+export const AdminFeatureBanner = () => {
+    return (
+        <Callout emoji="🔒">This feature is only available to team administrators.</Callout>
     );
 };
